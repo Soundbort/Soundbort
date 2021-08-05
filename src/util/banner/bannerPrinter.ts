2e35e8fa4d7727bf7324e6a0d53c6e9547890638
Loneless <LonelessCodes@users.noreply.github.com>
2021-08-05 20:02:41 +0200

    fix: banner file path fixed

--- src/util/banner/bannerPrinter.ts
+++ src/util/banner/bannerPrinter.ts
@@ -7,5 +7,5 @@ import { ENVIRONMENT } from "../../config";
 
 const package_json: PackageJson = fs.readJSONSync(path.join(process.cwd(), "package.json"));
 
-const txt = fs.readFileSync(path.join(__dirname, "..", "..", "..", "assets", "text", "banner.txt"), "utf8");
-console.log(txt, package_json.version, ENVIRONMENT, Discord.version, process.versions.node);
+const txt = fs.readFileSync(path.join(process.cwd(), "assets", "banner.txt"), "utf8");
+console.log(txt, package_json.version, ENVIRONMENT, Discord.version);
