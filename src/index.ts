1bb1dcbe51537156fd93fc24ffecef6690bcfdba
Loneless <LonelessCodes@users.noreply.github.com>
2021-08-09 08:06:18 +0200

    change: change boot-up status

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ const client = new Discord.Client({
     presence: {
         status: "dnd",
         activities: [{
-            name: "/soundbort | Booting...",
+            name: "Booting...",
             type: "PLAYING",
         }],
     },
