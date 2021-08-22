53b2bf5413f32e104b30c4707b8bc57a1e5d7ae9
Loneless <LonelessCodes@users.noreply.github.com>
2021-08-22 17:16:19 +0200

    refactor: remove unused intents

--- src/index.ts
+++ src/index.ts
@@ -16,10 +16,8 @@ const djs_log = Logger.child({ label: "discord.js" });
 
 const client = new Discord.Client({
     intents: [
-        Discord.Intents.FLAGS.DIRECT_MESSAGES,
         Discord.Intents.FLAGS.GUILDS,
         Discord.Intents.FLAGS.GUILD_VOICE_STATES,
-        Discord.Intents.FLAGS.GUILD_MESSAGES,
     ],
 
     presence: {
