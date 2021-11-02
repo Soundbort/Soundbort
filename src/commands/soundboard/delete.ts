e3a4f0fc73d4c358012d3966279b28151ef08563
Loneless <LonelessCodes@users.noreply.github.com>
2021-11-02 16:34:08 +0100

    change: change /delete behaviour (preparation for autocomplete)
    /delete now decides more smartly what sample the user wants to delete when they input the name or id

--- src/commands/soundboard/delete.ts
+++ src/commands/soundboard/delete.ts
@@ -1,68 +1,58 @@
 import Discord from "discord.js";
 
-import { BUTTON_TYPES, SAMPLE_TYPES } from "../../const";
+import { BUTTON_TYPES } from "../../const";
 
 import InteractionRegistry from "../../core/InteractionRegistry";
 import { CommandStringOption } from "../../modules/commands/CommandOption";
-import { createChoice } from "../../modules/commands/options/createChoice";
 import { TopCommand } from "../../modules/commands/TopCommand";
 import { EmbedType, replyEmbed, replyEmbedEphemeral } from "../../util/builders/embed";
 
 import { CustomSample } from "../../core/soundboard/CustomSample";
 import GuildConfigManager from "../../core/managers/GuildConfigManager";
 import SampleID from "../../core/soundboard/SampleID";
-
-async function removeServer(interaction: Discord.CommandInteraction, name: string) {
-    const userId = interaction.user.id;
-    const guildId = interaction.guildId;
-
-    if (!guildId || !interaction.guild) {
-        return replyEmbedEphemeral("You're not in a server.", EmbedType.Error);
-    }
-
-    if (!await GuildConfigManager.isModerator(interaction.guild, userId)) {
-        return replyEmbedEphemeral("You're not a moderator of this server, you can't remove server samples.", EmbedType.Error);
-    }
-
-    let sample = await CustomSample.findSampleGuild(guildId, name);
-    if (!sample && SampleID.isId(name)) {
-        sample = await CustomSample.findById(name);
-    }
-
-    if (!sample?.isInGuilds(guildId)) {
-        sample = undefined;
-    }
-
-    if (!sample) {
-        return replyEmbedEphemeral(`Couldn't find sample with name or id ${name}`, EmbedType.Error);
-    }
-
-    await CustomSample.remove(guildId, sample);
-
-    return replyEmbed(`Removed ${sample.name} (${sample.id}) from server soundboard!`, EmbedType.Success);
-}
-
-async function removeUser(interaction: Discord.CommandInteraction, name: string) {
-    const userId = interaction.user.id;
-
-    let sample = await CustomSample.findSampleUser(userId, name);
-    if (!sample && SampleID.isId(name)) {
-        sample = await CustomSample.findById(name);
-    }
-
-    if (!sample?.isInUsers(userId)) {
-        sample = undefined;
-    }
-
-    if (!sample) {
-        return replyEmbedEphemeral(`Couldn't find sample with name or id ${name}`, EmbedType.Error);
-    }
-
-    await CustomSample.remove(userId, sample);
-
-    return replyEmbed(`Removed ${sample.name} (${sample.id}) from user soundboard!`, EmbedType.Success);
-}
 import { createDialog, DialogOptionsButton } from "../../util/builders/dialog";
+
+async function dialogSameId(interaction: Discord.CommandInteraction, sampleId: string) {
+    await createDialog({
+        interaction,
+        dialog_text:
+            "This sample is part of your user and your server's soundboard. Where do you want to delete it from?",
+        dialog_type: EmbedType.Warning,
+        abort_type: BUTTON_TYPES.DELETE_ABORT,
+        buttons: [{
+            id: { id: sampleId, t: BUTTON_TYPES.DELETE_USER },
+            label: "Delete From User Board",
+            emoji: "🗑️",
+            style: "DANGER",
+        }, {
+            id: { id: sampleId, t: BUTTON_TYPES.DELETE_SERVER },
+            label: "Delete From Server Board",
+            emoji: "🗑️",
+            style: "DANGER",
+        }],
+    });
+}
+
+async function dialogSameName(interaction: Discord.CommandInteraction, userSampleId: string, guildSampleId: string) {
+    await createDialog({
+        interaction,
+        dialog_text:
+            "A sample with this name exists in both your user and your server soundboard. Which one do you want to delete?",
+        dialog_type: EmbedType.Warning,
+        abort_type: BUTTON_TYPES.DELETE_ABORT,
+        buttons: [{
+            id: { id: userSampleId, t: BUTTON_TYPES.DELETE_USER },
+            label: "Delete From User Board",
+            emoji: "🗑️",
+            style: "DANGER",
+        }, {
+            id: { id: guildSampleId, t: BUTTON_TYPES.DELETE_SERVER },
+            label: "Delete From Server Board",
+            emoji: "🗑️",
+            style: "DANGER",
+        }],
+    });
+}
 
 InteractionRegistry.addCommand(new TopCommand({
     name: "delete",
@@ -73,21 +63,116 @@ InteractionRegistry.addCommand(new TopCommand({
             description: "A sample name or sample identifier (sXXXXXX)",
             required: true,
         }),
-        new CommandStringOption({
-            name: "from",
-            description: "In case user or server samples have the same name.",
-            choices: [
-                createChoice("Only search server samples.", SAMPLE_TYPES.SERVER),
-                createChoice("Only search your user samples.", SAMPLE_TYPES.USER),
-            ],
-        }),
     ],
     async func(interaction) {
         const name = interaction.options.getString("sample", true).trim();
-        const scope = interaction.options.getString("from", false) as (SAMPLE_TYPES.USER | SAMPLE_TYPES.SERVER | null) || SAMPLE_TYPES.USER;
 
-        if (scope === SAMPLE_TYPES.USER) return await removeUser(interaction, name);
-        return await removeServer(interaction, name);
+        const userId = interaction.user.id;
+        const guildId = interaction.guildId;
+
+        // more of an experiment. the discord.js types have changed and are now partially wrong and less explicit
+        // even though the guildId is always of type Snowflake, it can be null when calling a global command from
+        // DMs
+        // next: if the guild is not cached, Interaction.guild is null, so try to fetch guild from discord. If not
+        // found, an error is thrown, and the command is aborted.
+        const guild = guildId && (interaction.guild || await interaction.client.guilds.fetch(guildId));
+        if (!guildId || !guild) {
+            return replyEmbedEphemeral("You're not in a server.", EmbedType.Error);
+        }
+
+        const is_admin = await GuildConfigManager.isModerator(guild, userId);
+
+        let sample: CustomSample | undefined;
+
+        // is the input a sample id?
+        if (SampleID.isId(name)) {
+            sample = await CustomSample.findById(name);
+        }
+
+        // is the input a sample id and a sample exists with that id?
+        if (sample) {
+            const is_in_guild = sample.isInGuilds(guildId);
+            const is_in_user = sample.isInUsers(userId);
+
+            if (!is_in_guild && !is_in_user) {
+                return replyEmbedEphemeral("You don't have this sample in your soundboard.", EmbedType.Info);
+            }
+
+            if (is_admin) {
+                if (is_in_guild && is_in_user) {
+                    await dialogSameId(interaction, sample.id);
+
+                    return;
+                }
+                if (is_in_user) {
+                    await CustomSample.remove(userId, sample);
+
+                    return replyEmbed(`Removed ${sample.name} (${sample.id}) from user soundboard!`, EmbedType.Success);
+                }
+                if (is_in_guild) {
+                    await CustomSample.remove(guildId, sample);
+
+                    return replyEmbed(`Removed ${sample.name} (${sample.id}) from server soundboard!`, EmbedType.Success);
+                }
+                // is_in_guild = false, is_in_user = false
+                return;
+            }
+
+            if (is_in_guild && !is_in_user) {
+                return replyEmbedEphemeral("You're not a moderator of this server, you can't remove server samples.", EmbedType.Error);
+            }
+
+            // (is_in_guild && is_in_user) || (is_in_user && !is_in_guild) || true
+
+            await CustomSample.remove(userId, sample);
+
+            return replyEmbed(`Removed ${sample.name} (${sample.id}) from user soundboard!`, EmbedType.Success);
+        }
+
+        // the input is not a valid sample id. check for samples with the input as name
+        const user_sample = await CustomSample.findSampleUser(userId, name);
+        const guild_sample = await CustomSample.findSampleGuild(guildId, name);
+
+        if (!user_sample && !guild_sample) {
+            return replyEmbedEphemeral(`Couldn't find sample with name or id ${name}`, EmbedType.Error);
+        }
+
+        if (is_admin) {
+            if (user_sample && guild_sample) {
+                if (user_sample.id === guild_sample.id) {
+                    await dialogSameId(interaction, user_sample.id);
+                } else {
+                    await dialogSameName(interaction, user_sample.id, guild_sample.id);
+                }
+                return;
+            }
+            if (user_sample) {
+                await CustomSample.remove(userId, user_sample);
+
+                return replyEmbed(`Removed ${user_sample.name} (${user_sample.id}) from user soundboard!`, EmbedType.Success);
+            }
+            if (guild_sample) {
+                await CustomSample.remove(guildId, guild_sample);
+
+                return replyEmbed(`Removed ${guild_sample.name} (${guild_sample.id}) from user soundboard!`, EmbedType.Success);
+            }
+            // guild_sample = undefined, user_sample = undefined
+            return;
+        }
+
+        if (guild_sample && !user_sample) {
+            return replyEmbedEphemeral("You're not a moderator of this server, you can't remove server samples.", EmbedType.Error);
+        }
+
+        // simply logical it's not possible for user_sample to be undefined at this point, but TS still thinks so,
+        // therefore put another if statement as a type guard
+        if (!user_sample) return;
+
+        // (is_in_guild && is_in_user) || (!is_in_guild && is_in_user) || true
+
+        await CustomSample.remove(userId, user_sample);
+
+        return replyEmbed(`Removed ${user_sample.name} (${user_sample.id}) from user soundboard!`, EmbedType.Success);
     },
 }));
 
