dd6262d5b65af3fd50955b92e8dfcceb0e1b124d
Loneless <LonelessCodes@users.noreply.github.com>
2021-11-03 19:10:24 +0100

    refactor: remove all console.log

--- src/util/banner_printer.ts
+++ src/util/banner_printer.ts
@@ -1,6 +1,7 @@
 import Discord from "discord.js";
 import fs from "fs-extra";
 import path from "node:path";
+import { format } from "node:util";
 import { PackageJson } from "type-fest";
 
 import { ASSETS_DIR, ENVIRONMENT, PROJECT_ROOT } from "../config.js";
@@ -8,4 +9,4 @@ import { ASSETS_DIR, ENVIRONMENT, PROJECT_ROOT } from "../config.js";
 const package_json: PackageJson = fs.readJSONSync(path.join(PROJECT_ROOT, "package.json"));
 
 const txt = fs.readFileSync(path.join(ASSETS_DIR, "banner.txt"), "utf8");
-console.log(txt, package_json.version, ENVIRONMENT, Discord.version);
+process.stdout.write(format(txt, package_json.version, ENVIRONMENT, Discord.version, "\n"));
