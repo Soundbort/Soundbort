e3f6797ad95628bf860bfd2bceb3bc7382bb54cf
Christian Schäfer <14093936+LonelessCodes@users.noreply.github.com>
2022-05-15 10:15:48 +0200

    refactor: remove unused code

--- src/util/types.ts
+++ src/util/types.ts
@@ -1,9 +1,5 @@
 import * as Discord from "discord.js";
 
-export interface KeyValue<V> {
-    [code: string]: V | undefined;
-}
-
 export interface CmdInstallerArgs {
     client: Discord.Client<true>;
 }
